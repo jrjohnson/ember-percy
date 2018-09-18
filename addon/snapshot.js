7d5cb88ffe6190ed8234cb1e2b9050ae8d459ec2
David Jones <dgjones@gmail.com>
2018-09-18 13:46:20 -0700

    support default environment breakpoints and PERCY_ENABLE=0

--- addon/snapshot.js
+++ addon/snapshot.js
@@ -1,23 +1,26 @@
+/* eslint-disable no-console */
+
 import { run } from '@ember/runloop';
-import percyJQuery from 'percy-jquery';
+// import percyJQuery from 'percy-jquery';
 import { getNativeXhr } from './native-xhr';
+import config from 'ember-get-config';
 
 // Copy attributes from Ember's rootElement to the DOM snapshot <body> tag. Some applications rely
 // on setting attributes on the Ember rootElement (for example, to drive dynamic per-route
 // styling). In tests these attributes are added to the #ember-testing container and would be lost
 // in the DOM hoisting, so we copy them to the to the snapshot's <body> tag to
 // make sure that they persist in the DOM snapshot.
-function copyAttributesToBodyCopy(bodyCopy, testingContainer) {
-  let attributesToCopy = testingContainer.prop('attributes');
-  percyJQuery.each(attributesToCopy, function() {
-    // Special case for the class attribute - append new classes onto existing body classes
-    if (this.name === 'class') {
-      bodyCopy.attr(this.name, bodyCopy.attr('class') + ' ' + this.value);
-    } else {
-      bodyCopy.attr(this.name, this.value);
-    }
-  });
-}
+// function copyAttributesToBodyCopy(bodyCopy, testingContainer) {
+//   let attributesToCopy = testingContainer.prop('attributes');
+//   percyJQuery.each(attributesToCopy, function() {
+//     // Special case for the class attribute - append new classes onto existing body classes
+//     if (this.name === 'class') {
+//       bodyCopy.attr(this.name, bodyCopy.attr('class') + ' ' + this.value);
+//     } else {
+//       bodyCopy.attr(this.name, this.value);
+//     }
+//   });
+// }
 
 function buildFullName(name) {
   // Automatic name generation for QUnit tests by passing in the `assert` object.
@@ -34,7 +37,16 @@ function buildFullName(name) {
 export function percySnapshot(name, options) {
   // Skip if Testem is not available (we're probably running from `ember server`
   // and Percy is not enabled anyway).
-  if (!window.Testem || process.env.PERCY_ENABLE == '0') { return; }
+  if (!window.Testem) { return; }
+
+  // Skip if PERCY_ENABLE is set to 0
+  if (config.env.PERCY_ENABLE === '0') { return; }
+
+  var snapshotOptions = options || {};
+
+  if (!snapshotOptions.widths) {
+    snapshotOptions.widths = config.percy.defaultWidths;
+  }
 
   var fullName = buildFullName(name);
 
@@ -51,7 +63,7 @@ export function percySnapshot(name, options) {
     );
 
     run(function () {
-      percy.snapshot(fullName, options);
+      percy.snapshot(fullName, snapshotOptions);
     });
   } catch (e) {
     console.log('WARNING! percy-agent is not started. See https://docs.percy.io/docs for help.');
